21592e1fe6db74aefc37e6024c2651a83b0e4ca9
river <phamtrungnghia2004@gmail.com>
2024-09-15 17:22:48 +0700

    Reverse back-end deletion

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,22 +1,9 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import users from '../models/users';
+import users from '../models/users.js';
 import nodemailer from 'nodemailer';
-import google from 'googleapis';
-import {
-  SEND_MAIL_CLIENT_ID,
-  SEND_MAIL_CLIENT_SECRET,
-  SEND_MAIL_REDIRECT_URL,
-  JWT_SECRET,
-  EMAIL_USER,
-  SEND_MAIL_REFRESH_TOKEN,
-} from '@env';
-
-const OAuth2Client = new google.auth.OAuth2(
-  SEND_MAIL_CLIENT_ID,
-  SEND_MAIL_CLIENT_SECRET,
-  SEND_MAIL_REDIRECT_URL,
-);
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const signIn = async (req, res) => {
   const {email, password} = req.body;
@@ -37,7 +24,7 @@ export const signIn = async (req, res) => {
 
     const token = jwt.sign(
       {email: existingUser.email, id: existingUser._id},
-      JWT_SECRET,
+      process.env.JWT_SECRET,
     );
     res.status(200).json({result: existingUser, token});
   } catch (error) {
@@ -53,17 +40,24 @@ export const signUp = async (req, res) => {
       return res.status(404).json({message: 'User already existed'});
     }
 
-    const salt = await bcrypt.genSaltSync();
-    const hashedPassword = await bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSaltSync();
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-    // const result = await users.create({email, password: hashedPassword, name, DateOfBirth: dob});
-    const result = await users.create({email, password: hashedPassword, name});
+        // const result = await users.create({email, password: hashedPassword, name, DateOfBirth: dob});
+        let result;
+        if (!dob){
+          result = await users.create({email, password: hashedPassword, name});
+        }
+        else {
+          result = await users.create({email, password: hashedPassword, name, DateOfBirth: dob});
+        }
 
-    const token = jwt.sign({email: result.email, id: result._id}, JWT_SECRET);
-    res.status(200).json({user: result, token});
-  } catch (error) {
-    res.status(500).json({message: 'Something went wrong!'});
-  }
+        const token = jwt.sign({email: result.email, id: result._id}, process.env.JWT_SECRET);
+        res.status(200).json({user: result, token});
+    } catch (error) {
+        users.deleteOne({email});
+        res.status(500).json({message: 'Something went wrong!'});
+    }
 };
 
 const generate6DigitCode = () => {
@@ -86,26 +80,21 @@ export const sendCode = async (req, res) => {
     User.verificationCodeExpires = verificationCodeExpires;
     await User.save();
 
-    const accessToken = await OAuth2Client.getAccessToken();
-
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      service: 'hotmail',
       auth: {
-        type: 'OAuth2',
-        user: EMAIL_USER,
-        clientId: SEND_MAIL_CLIENT_ID,
-        clientSecret: SEND_MAIL_CLIENT_SECRET,
-        refreshToken: SEND_MAIL_REFRESH_TOKEN,
-        accessToken: accessToken,
+        user: 'MeFiMemorii@outlook.com.vn',
+        pass: 'MeFi@12369874',
       },
     });
 
     const message = {
-      from: 'Memorii <' + process.env.EMAIL_USER + '>',
-      to: User.email,
+      from: 'Memorii <MeFiMemorii@outlook.com.vn>',
+      to: email,
       subject: 'Verify gmail users',
       text: 'Your verification code for Memorii app is ${verificationCode}. This code will be expired after ${expiredMin} minutes',
     };
+
     await transporter.sendMail(message);
     res.status(200).json({user: User});
   } catch (error) {
@@ -122,10 +111,20 @@ export const verify = async (req, res) => {
     }
 
     const generatedCode = User.verificationCode;
+    const expired = User.verificationCodeExpires;
     let verified = false;
-    if (generatedCode === verifiedCode) {
+    if (generatedCode === verifiedCode && expired > Date.now()) {
       verified = true;
     }
+
+    if (!verified) {
+      return res.status(404).json({ message: 'Invalid or expired code' });
+    }
+
+    User.verificationCode = null;
+    User.verificationCodeExpires = null;
+    User.verified = true;
+    await User.save();
 
     res.status(200).json({user: User, verified});
   } catch (error) {
