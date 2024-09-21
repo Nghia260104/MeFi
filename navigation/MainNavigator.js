65a3ce706b35a067bd3bec7477ff463c4428efff
To Ngoc Hung <103498379+Kas1902@users.noreply.github.com>
2024-09-21 23:14:59 +0700

    handleKeepLogin

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,6 +1,11 @@
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useState} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {View, ActivityIndicator} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useSelector} from 'react-redux';
 
+import {USER_KEY} from '@env';
 import Onboarding from '../screens/Onboarding/Onboarding';
 import Sex from '../screens/Sex/Sex';
 import LogIn from '../screens/LogIn/LogIn';
@@ -13,17 +18,58 @@ import Carousel2 from '../screens/Home/Carousel2';
 import CycleJournal from '../screens/Home/CycleJournal';
 import Personal from '../screens/PersonalInfo.js/Personal';
 import Info from '../screens/PersonalInfo.js/Info';
-import {useSelector} from 'react-redux';
+import LottieView from 'lottie-react-native';
+import {horizontalScale} from '../assets/styles/scaling';
 
 const Stack = createStackNavigator();
 
 const MainNavigator = () => {
   const alreadyLaunched = useSelector(state => state.check.alreadyLaunched);
+  const [initialRoute, setInitialRoute] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchStoredData = async () => {
+      try {
+        const data = await AsyncStorage.getItem(USER_KEY);
+        if (data) {
+          const parsedData = JSON.parse(data);
+          if (parsedData.token) {
+            setInitialRoute('Main');
+          } else {
+            setInitialRoute(alreadyLaunched ? 'LogIn' : 'Onboarding');
+          }
+        } else {
+          setInitialRoute(alreadyLaunched ? 'LogIn' : 'Onboarding');
+        }
+      } catch (error) {
+        console.error('Failed to fetch stored data:', error);
+        setInitialRoute(alreadyLaunched ? 'LogIn' : 'Onboarding');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchStoredData();
+  }, [alreadyLaunched]);
+
+  if (isLoading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <LottieView
+          style={{width: horizontalScale(200), height: horizontalScale(200)}}
+          source={require('../assets/images/Loading.json')}
+          autoPlay
+          loop
+        />
+      </View>
+    );
+  }
 
   return (
     <Stack.Navigator
       screenOptions={{header: () => null, headerShown: false}}
-      initialRouteName={alreadyLaunched ? 'Sex' : 'Onboarding'}>
+      initialRouteName={initialRoute}>
       <Stack.Screen name="Onboarding" component={Onboarding} />
       <Stack.Screen name="Sex" component={Sex} />
       <Stack.Screen name="LogIn" component={LogIn} />
